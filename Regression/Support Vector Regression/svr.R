71f5e549509043730176a9a7373bacc58c0a64ab
RITWIK JHA <ritwik0706@gmail.com>
2018-12-16 16:45:15 +0530

    SVR in R

--- Regression/Support Vector Regression/svr.R
+++ Regression/Support Vector Regression/svr.R
@@ -1,26 +1,27 @@
 
-# Regression Template
-
-
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
 # Splitting the dataset into the Training set and Test set
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# # install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fiitng the Polynomial Regression Mopdel
-# Create the Regressor
+# Fitting the Regression Model to the dataset
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset, 
+                type = 'eps-regression')
 
-# Predicting Polynomial Regression Results
+# Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
 # Visualising the Regression Model results
@@ -47,4 +48,3 @@ ggplot() +
   ggtitle('Truth or Bluff (Regression Model)') +
   xlab('Level') +
   ylab('Salary')
-
